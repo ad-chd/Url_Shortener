dab981bbeafd0c9b58890162bee7371d34a7d4d1
Aditya Chaudhary <72156890+ad-chd@users.noreply.github.com>
2023-06-29 03:48:57 +0530

    Update LogsPage.js

--- src/components/LogsPage.js
+++ src/components/LogsPage.js
@@ -1,30 +1,37 @@
 import React from 'react';
 
 const LogsPage = ({ shortenedLinks, deleteLink, handleClick }) => {
+  // Check if shortenedLinks is undefined or null
+  if (!shortenedLinks) {
+    return null; // or render a loading indicator, an error message, or an empty state
+  }
+
   return (
     <div>
-      <h2>URL History</h2>
+    <h2 style={{ color: '#39FF14' }}>URL History</h2>
       <table>
         <thead>
           <tr>
-            <th>Original URL</th>
-            <th>Shortened URL</th>
-            <th>Visits</th>
-            <th>Action</th>
+            <th style={{ backgroundColor: '#f0f0f0' }}>Original URL</th>
+            <th style={{ backgroundColor: '#f0f0f0' }}>Shortened URL</th>
+            <th style={{ backgroundColor: '#f0f0f0' }}>Visits</th>
+            <th style={{ backgroundColor: '#f0f0f0' }}>Action</th>
           </tr>
         </thead>
         <tbody>
           {shortenedLinks.map((link, index) => (
             <tr key={index}>
-              <td>{link.originalUrl}</td>
-              <td>
+              <td style={{ backgroundColor: '#ffffff' }}>{link.originalUrl}</td>
+              <td style={{ backgroundColor: '#ffffff' }}>
                 <a href={link.shortenedUrl} target="_blank" rel="noopener noreferrer">
                   {link.shortenedUrl}
                 </a>
               </td>
-              <td>{link.visits}</td>
+              <td style={{ backgroundColor: '#ffffff' }}>{link.visits}</td>
               <td>
-                <button onClick={() => deleteLink(index)}>Delete</button>
+                <button style={{ backgroundColor: '#ff0000', color: '#ffffff' }} onClick={() => deleteLink(index)}>
+                  Delete
+                </button>
               </td>
             </tr>
           ))}
